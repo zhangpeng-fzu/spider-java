028e06d0feb5a83a83637ce13bed2e354b31af7f
zhangpeng <1025711995@qq.com>
2020-11-05 21:02:16 +0800

    优化进球分析

--- fb_livescore/src/main/java/com/peng/frame/panel/MatchNumPanelFactory.java
+++ fb_livescore/src/main/java/com/peng/frame/panel/MatchNumPanelFactory.java
@@ -2,6 +2,7 @@ package com.peng.frame.panel;
 
 import com.peng.bean.MatchBean;
 import com.peng.bean.MatchNumBean;
+import com.peng.bean.MissValueDataBean;
 import com.peng.constant.Constants;
 import com.peng.repository.LiveDataRepository;
 import com.peng.repository.MatchNumRepository;
@@ -10,10 +11,9 @@ import com.peng.util.DateUtil;
 import javax.swing.*;
 import java.awt.*;
 import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.HashMap;
+import java.text.ParseException;
 import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MatchNumPanelFactory extends PaneFactory {
     private static MatchNumPanelFactory matchNumPanelFactory;
@@ -30,6 +30,119 @@ public class MatchNumPanelFactory extends PaneFactory {
         return !Constants.MATCH_STATUS_MAP.containsKey(matchNum) ||
                 (DateUtil.isToday(date) && (!isPlaying(Constants.MATCH_STATUS_MAP.get(matchNum)) || isCancelled(Constants.MATCH_STATUS_MAP.get(matchNum))))
                 || (!DateUtil.isToday(date) && (isUnFinished(Constants.MATCH_STATUS_MAP.get(matchNum)) || isCancelled(Constants.MATCH_STATUS_MAP.get(matchNum))));
+    }
+
+    public MissValueDataBean getMissValueData(String matchNum, boolean statistics) throws ParseException {
+        String[] columnNames = Constants.MATCH_NUM_COLUMNS_DATE;
+        int size = columnNames.length;
+        int row = 0;
+
+        List<MatchBean> matchList = LiveDataRepository.getMatchListByNum(matchNum);
+
+        //最大行数，中间可能有些行无需显示
+        int maxRow = statistics ? matchList.size() + 3 : matchList.size();
+
+        String[][] tableData = new String[maxRow][size];
+        String[] lastMissValues = new String[size - 1];
+        Arrays.fill(lastMissValues, "0");
+        int[] matchCompareCountArr = new int[size - 1];
+        int[] matchCompareMaxArr = new int[size - 1];
+        int[] matchCompareMax300Arr = null;
+
+        String today = DateUtil.getDateFormat().format(new Date());
+
+        for (int index = 0; index < matchList.size(); index++) {
+            MatchBean matchBean = matchList.get(index);
+
+            //以往，未完成或者已取消的场次
+            if (!matchBean.getLiveDate().equals(today) && isUnFinished(matchBean.getStatus())) {
+                if (!statistics) {
+                    row++;
+                }
+                continue;
+            }
+
+            //当天未完成的场次 显示空行
+            if (matchBean.getLiveDate().equals(today)) {
+                tableData[row] = new String[size];
+                tableData[row][0] = DateUtil.getDateFormat(1).format(DateUtil.getDateFormat().parse(matchBean.getLiveDate()));
+                for (int i = 1; i < columnNames.length; i++) {
+                    tableData[row][i] = "";
+                }
+                row++;
+                continue;
+            }
+
+            if (matchCompareMax300Arr == null && matchList.size() - index <= 300) {
+                matchCompareMax300Arr = new int[size - 1];
+            }
+
+            String[] columns = new String[size - 1];
+            System.arraycopy(columnNames, 1, columns, 0, columns.length);
+
+            String[] missValues = calcMissValue(matchBean, columns, lastMissValues, matchCompareCountArr, matchCompareMaxArr, matchCompareMax300Arr);
+            lastMissValues = missValues;
+
+            tableData[row] = new String[size];
+            tableData[row][0] = DateUtil.getDateFormat(1).format(DateUtil.getDateFormat().parse(matchBean.getLiveDate()));
+
+            System.arraycopy(missValues, 0, tableData[row], 1, missValues.length);
+            row++;
+        }
+        if (statistics) {
+            //增加统计数据
+            addStatisticsData(row, size, tableData, matchCompareCountArr, matchCompareMaxArr, null, 1, 1);
+            row = row + 3;
+        }
+        return MissValueDataBean.builder().missValueData(tableData).row(row).build();
+    }
+
+    private String[] calcMissValue(MatchBean matchBean, String[] columns, String[] lastMissValues, int[] matchCountArr, int[] matchMaxArr, int[] matchMax300Arr) throws ParseException {
+
+        String[] missValues = new String[lastMissValues.length];
+
+        int matchNum = matchBean.getNum();
+
+        for (int i = 0; i < columns.length; i++) {
+
+            if (isHit(matchNum, i)) {
+                missValues[i] = "中";
+                if (matchCountArr != null) {
+                    matchCountArr[i]++;
+                }
+            } else {
+                missValues[i] = String.valueOf(Integer.parseInt(lastMissValues[i].equals("中") ? "0" : lastMissValues[i]) + 1);
+                if (matchMaxArr != null) {
+                    matchMaxArr[i] = Math.max(matchMaxArr[i], Integer.parseInt(missValues[i]));
+                }
+                if (matchMax300Arr != null) {
+                    matchMax300Arr[i] = Math.max(matchMax300Arr[i], Integer.parseInt(missValues[i]));
+                }
+            }
+        }
+        return missValues;
+    }
+
+
+    private boolean isHit(int matchNum, int column) {
+        switch (matchNum) {
+            case 0:
+                return column == 0 || column == 8;
+            case 1:
+                return column == 1 || column == 9;
+            case 2:
+                return column == 2 || column == 10;
+            case 3:
+                return column == 3 || column == 9;
+            case 4:
+                return column == 4 || column == 10;
+            case 5:
+                return column == 5 || column == 11;
+            case 6:
+                return column == 6 || column == 11;
+            default:
+                return column == 7 || column == 11;
+        }
     }
 
     /**
