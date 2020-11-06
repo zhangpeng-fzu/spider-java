2d33bcfddf26cd660a1e70b994490673d60a7d04
pengzhang28 <pengzhang28>
2020-11-06 18:32:42 +0800

    重构

--- fb_livescore/src/main/java/com/peng/frame/panel/MatchComparePanelFactory.java
+++ fb_livescore/src/main/java/com/peng/frame/panel/MatchComparePanelFactory.java
@@ -9,8 +9,10 @@ import com.peng.util.DateUtil;
 import javax.swing.*;
 import java.awt.*;
 import java.text.ParseException;
-import java.util.List;
-import java.util.*;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MatchComparePanelFactory extends PaneFactory {
@@ -21,83 +23,36 @@ public class MatchComparePanelFactory extends PaneFactory {
     }
 
 
-    private MissValueDataBean getMissValueData(String matchNum, boolean statistics) throws ParseException {
-        String[] columnNames = Constants.MATCH_COMPARE_COLUMNS;
-        int size = columnNames.length;
-        int row = 0;
-        int compareNum = (columnNames.length - 4) / 2;
-        String today = DateUtil.getDateFormat().format(new Date());
-
-
-        List<MatchBean> matchList = LiveDataRepository.getMatchListByNum(matchNum);
-        boolean hasToday = matchList.stream().anyMatch(matchBean -> today.equals(matchBean.getLiveDate()));
-
-        if (!hasToday) {
-            matchList.add(MatchBean.builder().liveDate(today).build());
-        }
-        int maxRow = statistics ? matchList.size() + 3 : matchList.size();
-        String[][] tableData = new String[maxRow][size];
-        String[] lastMissValues = new String[size - 4];
-        Arrays.fill(lastMissValues, "0");
-
-        //统计数据
-        int[] matchCompareCountArr = new int[compareNum];
-        int[] matchCompareMaxArr = new int[compareNum];
-        int[] matchCompareMax300Arr = null;
-
-        String[] curCompareData;
-        for (int index = 0; index < matchList.size(); index++) {
-            MatchBean matchBean = matchList.get(index);
-            curCompareData = Constants.INIT_COMPARE_DATA[index % Constants.INIT_COMPARE_DATA.length];
-            //当天未完成的场次 显示空行
-            if (matchBean.getLiveDate().equals(today)) {
-                tableData[row] = new String[size];
-                tableData[row][0] = DateUtil.getDateFormat(1).format(DateUtil.getDateFormat().parse(today));
-                for (int i = 1; i < columnNames.length; i++) {
-                    if (i >= 4 && i % 2 == 0) {
-                        tableData[row][i] = curCompareData[(i - 4) / 2];
-                    } else {
-                        tableData[row][i] = "";
-                    }
-                }
-                row++;
-                continue;
-            }
-            //以往，未完成或者已取消的场次
-            if (!matchBean.getLiveDate().equals(today) && isUnFinished(matchBean.getStatus())) {
-                continue;
-            }
-
-
-            if (matchCompareMax300Arr == null && matchList.size() - index <= 300) {
-                matchCompareMax300Arr = new int[compareNum];
-            }
-
-            String[] missValues = calcMissValue(matchBean, curCompareData, lastMissValues, matchCompareCountArr, matchCompareMaxArr, matchCompareMax300Arr);
-            lastMissValues = missValues;
-
-            tableData[row] = new String[size];
-            tableData[row][0] = DateUtil.getDateFormat(1).format(DateUtil.getDateFormat().parse(matchBean.getLiveDate()));
-            tableData[row][1] = String.format("%s:%s", matchBean.getHostNum(), matchBean.getGuestNum());
-            tableData[row][2] = String.valueOf(matchBean.getNum());
-            tableData[row][3] = matchBean.getMatchStatus();
-
-            System.arraycopy(missValues, 0, tableData[row], 4, missValues.length);
-            row++;
-        }
-
-
-        if (statistics) {
-            //增加统计数据
-            addStatisticsData(row, size, tableData, matchCompareCountArr, matchCompareMaxArr, matchCompareMax300Arr, 2, 5);
-            row = row + 3;
-        }
-        String[][] newTableData = new String[row][size];
-        System.arraycopy(tableData, 0, newTableData, 0, row);
-        return MissValueDataBean.builder().missValueData(newTableData).build();
+    @Override
+    public String[] getColumns(int index, String[] columnNames, int offset) {
+        return Constants.INIT_COMPARE_DATA[index % Constants.INIT_COMPARE_DATA.length];
     }
 
-    private String[] calcMissValue(MatchBean matchBean, String[] curCompareData, String[] lastMissValues, int[] matchCompareCountArr, int[] matchCompareMaxArr, int[] matchCompareMax300Arr) {
+    @Override
+    public void fillTodayData(String[] tableRow, String[] columnNames, String[] curCompareData, int step, int offset) throws ParseException {
+        tableRow[0] = DateUtil.getDateFormat(1).format(DateUtil.getDateFormat().parse(DateUtil.getDateFormat().format(new Date())));
+        for (int i = 1; i < columnNames.length; i++) {
+            if (i >= offset && i % step == 0) {
+                tableRow[i] = curCompareData[(i - offset) / step];
+            } else {
+                tableRow[i] = "";
+            }
+        }
+    }
+
+
+    @Override
+    public void fillTableData(String[] tableRow, String[] missValues, MatchBean matchBean) throws ParseException {
+        tableRow[0] = DateUtil.getDateFormat(1).format(DateUtil.getDateFormat().parse(matchBean.getLiveDate()));
+        tableRow[1] = String.format("%s:%s", matchBean.getHostNum(), matchBean.getGuestNum());
+        tableRow[2] = String.valueOf(matchBean.getNum());
+        tableRow[3] = matchBean.getMatchStatus();
+
+        System.arraycopy(missValues, 0, tableRow, 4, missValues.length);
+    }
+
+    @Override
+    public String[] calcMissValue(MatchBean matchBean, String[] curCompareData, String[] lastMissValues, int[] matchCompareCountArr, int[] matchCompareMaxArr, int[] matchCompareMax300Arr) {
 
         String[] missValues = new String[lastMissValues.length];
         System.arraycopy(lastMissValues, 0, missValues, 0, lastMissValues.length);
@@ -146,7 +101,7 @@ public class MatchComparePanelFactory extends PaneFactory {
             }
             rowData[column][0] = matchNum;
 
-            MissValueDataBean missValueDataBean = this.getMissValueData(matchNum, true);
+            MissValueDataBean missValueDataBean = this.getMissValueData(matchNum, true, Constants.COMPARE_TABLE, 1, 1);
             String[][] missValueData = missValueDataBean.getMissValueData();
 
             //合并今天和昨天的数据
@@ -174,7 +129,7 @@ public class MatchComparePanelFactory extends PaneFactory {
 
     JScrollPane showMatchPaneByNum(String matchNum) throws ParseException {
         String[] columnNames = Constants.MATCH_COMPARE_COLUMNS;
-        MissValueDataBean missValueDataBean = this.getMissValueData(matchNum, true);
+        MissValueDataBean missValueDataBean = this.getMissValueData(matchNum, true, Constants.COMPARE_TABLE, 1, 1);
         String[][] tableData = missValueDataBean.getMissValueData();
         int size = columnNames.length;
         JTable table = new JTable(tableData, columnNames);
